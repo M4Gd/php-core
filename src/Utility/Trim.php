e9ee285ba01e820a39564691456c552c280f4ff0
Majid <mj_azani@hotmail.com>
2021-06-08 12:11:35 +0430

    Updating the trim text method

--- src/Utility/Trim.php
+++ src/Utility/Trim.php
@@ -12,9 +12,14 @@ class Trim
 	 *
 	 * @return string
 	 */
-    public static function text( $string, $max_length = 1000, $more = ' ...' )
-    {
-        return function_exists( 'mb_strimwidth' ) ? mb_strimwidth( $string, 0, $max_length, $more ) : substr( $string, 0, $max_length ) . $more;
-    }
+    public static function text( $string, $max_length = 1000, $more = " ..." ){
+		$string_length = function_exists('mb_strwidth') ? mb_strwidth( $string ) : strlen( $string );
+
+		if( $string_length > $max_length && ! empty( $max_length ) ){
+			return function_exists( 'mb_strimwidth' ) ? mb_strimwidth( $string, 0, $max_length, '' ) . $more : substr( $string, 0, $max_length ) . $more;
+		}
+
+		return $string;
+	}
 
 }
