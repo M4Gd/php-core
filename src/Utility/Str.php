a667a917e38a4e6ae33cde17b17f28e71370a361
Majid <mj_azani@hotmail.com>
2021-06-09 14:26:05 +0430

    camelize method added to Str utility class

---
+++ src/Utility/Str.php
@@ -0,0 +1,27 @@
+<?php
+namespace Averta\Core\Utility;
+
+
+class Str
+{
+	/**
+	 * Convert string to camelCase
+	 *
+	 * @param string  $input                     The string to convert
+	 * @param string  $separator                 The character that should be removed
+	 * @param boolean $capitalizeFirstCharacter  Whether to capitalize the first character or not
+	 *
+	 * @return string
+	 */
+	public static function camelize( string $input, string $separator = "_", $capitalizeFirstChar = false ) {
+
+		$string = str_replace( $separator, '', ucwords( $input, $separator ) );
+
+		if ( ! $capitalizeFirstChar ) {
+			$string = lcfirst( $string );
+		}
+
+		return $string;
+	}
+
+}
